62b59919fdaf44812aba692d7f41124aa41a70e3
NelanJoe <nelanjorelat@gmail.com>
2022-11-28 19:24:57 +0700

    add: function in app.py

--- pertemuan5/app.py
+++ pertemuan5/app.py
@@ -4,5 +4,15 @@
 # Buatlah fungsi yang bisa membuat perkalian
 # Buatlah fungsi yang bisa membuat pembagian
 
+def perkalian(bilanganPertama, bilanganKedua):
+    total = bilanganPertama * bilanganKedua
+    return total
+
+def pembagian(bilanganPertama, bilanganKedua):
+    total = int(bilanganPertama / bilanganKedua)
+    return total
 
 
+
+print(perkalian(10, 5))
+print(pembagian(10, 2))
