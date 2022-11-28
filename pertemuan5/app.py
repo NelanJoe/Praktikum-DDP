140988b950c1474e4664d2962f051528c4fd3de1
NelanJoe <nelanjorelat@gmail.com>
2022-11-28 16:12:39 +0700

    intial commit

---
+++ pertemuan5/app.py
@@ -0,0 +1,8 @@
+'''
+    * Challange
+'''
+# Buatlah fungsi yang bisa membuat perkalian
+# Buatlah fungsi yang bisa membuat pembagian
+
+
+
---
+++ pertemuan5/function.py
@@ -0,0 +1,18 @@
+'''
+    * Function
+    *  kode program yang dirancang untuk menyelesaikan sebuah tugas tertentu, dan merupakan bagian dari program utama
+'''
+
+def hello():
+    print("Hello, World")
+
+
+# Function dengan parameter
+
+def sayHello(name):
+    print(f"Hello, nama saya {name}")
+
+
+def tambah(bilanganPertama, bilanganKedua):
+    total = bilanganPertama + bilanganKedua
+    print(total)
