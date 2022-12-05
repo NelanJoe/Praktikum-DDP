8001412464f130248fdaa70ce5329c4744d64e82
NelanJoe <nelanjorelat@gmail.com>
2022-12-05 18:40:12 +0700

    init

--- pertemuan6/jadwal_sholat.py
+++ pertemuan6/jadwal_sholat.py
@@ -1,25 +1,72 @@
+'''
+    * Import modul Beautifulsoup4
+    * Modul didapat dari install modul: pip install beautifulsoup4
+
+    * Import modul requests
+    * Modul didapat dari install modul: pip install requests
+'''
 from bs4 import BeautifulSoup
 import requests
 
 def jadwal_sholat(url):
+    '''
+        * Definisikan variabel sholat:
+        * - subuh
+        * - dzuhur
+        * - ashar
+        * - maghrib
+        * - isya
+    '''
+    subuh = ""
+    dzuhur = ""
+    ashar = ""
+    maghrib = ""
+    isya = ""
+
+    '''
+        * Coba get koneksi ke url
+        * Jika gagal maka 
+    '''
     try:
         content = requests.get(url)
+        print("Berhasil Koneksi Ke URL")
     except Exception:
+        print("Gagal Koneksi Ke URL")
         return None
 
-    if content.status_code == 200:
+    '''
+        * Jika status code dari content == 200 maka jalankan body if
+    '''
+    if (content.status_code == 200):
+        
+        # Buat variabel soup dengan function built-in BeautifulSoup
         soup = BeautifulSoup(content.text, "html.parser")
 
+        '''
+            * Definisikan variabel jadwal_all
+            * dengan isinya dari variabel soup dengan memanggil function built-in find_all
+        '''
         jadwal_all = soup.find_all("p", {"class": "praytime"})
         
-        subuh = jadwal_all[0].text
-        dzuhur = jadwal_all[1].text
-        ashar = jadwal_all[2].text
-        maghrib = jadwal_all[3].text
-        isya = jadwal_all[3].text
+        i = 0
+        for jadwal in jadwal_all:
+            if i == 1:
+                subuh = jadwal.text
+            elif i == 2:
+                dzuhur = jadwal.text
+            elif i == 3:
+                ashar = jadwal.text
+            elif i == 4:
+                maghrib = jadwal.text
+            elif i == 5:
+                isya = jadwal.text
 
-    print(f"Waktu Sholat Subuh: {subuh}")
-    print(f"Waktu Sholat Dzuhur: {dzuhur}")
-    print(f"Waktu Sholat Ashar: {ashar}")
-    print(f"Waktu Sholat Maghrib: {maghrib}")
-    print(f"Waktu Sholat Isya: {isya}")
+            i += 1
+
+
+    print(f"Waktu Sholat Subuh   : {subuh} WIB")
+    print(f"Waktu Sholat Dzuhur  : {dzuhur} WIB")
+    print(f"Waktu Sholat Ashar   : {ashar} WIB")
+    print(f"Waktu Sholat Maghrib : {maghrib} WIB")
+    print(f"Waktu Sholat Isya    : {isya} WIB")
+    
--- pertemuan6/main.py
+++ pertemuan6/main.py
@@ -1,5 +1,12 @@
-from jadwalsholat import jadwal_sholat
+'''
+    * import modul dari file jadwal_sholat.py
+    * import function jadwal_sholat
+'''
 
+from jadwal_sholat import jadwal_sholat
+
+# Definisikan URL / alamat dari sebuah situs
 url = "https://www.muslimpro.com/id/"
 
+# Panggil function jadwal_sholat
 jadwal_sholat(url)
